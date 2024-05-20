17cb5044339a866cc34db48e723b07b6b5cdfdd5
nopriterbang <novalpriya@gmail.com>
2024-05-20 16:07:55 +0700

    menambahkan class manusia

--- komposisi/manusia.h
+++ komposisi/manusia.h
@@ -2,3 +2,15 @@
 #define MANUSIA_H
 using namespace std;
 
+class Manusia {
+    public:
+    string nama;
+    jantung varJantung
+
+    Manusia(string pName) : name(pName){
+        cout << name << "hidup\n";
+    }
+    ~Manusia(){
+        cout << name << "hidup\n";
+    }
+}
