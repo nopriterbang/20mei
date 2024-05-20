112e2578c82dfd86d169bddfb9a0ce837e77d6b2
nopriterbang <novalpriya@gmail.com>
2024-05-20 18:44:16 +0700

    menambahkan int main

--- inheritance/inheritance.cpp
+++ inheritance/inheritance.cpp
@@ -34,3 +34,11 @@ class pelajar : public orang {
             return "Halo, nama saya " + nama + " dari sekolah " + sekolah + "\n \n";
         }
 };
+
+int main() {
+    pelajar siswa1("Andi Aksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}
